de439af9843c78de218216d2731858f883147dc5
CharlyAuzanet <charly.auzanet@gmail.com>
2017-05-23 20:19:13 +0200

    vue controleur et route créée pour les vues question demo + minimum de css

---
+++ src/InseadBundle/Controller/QuestionDemoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace InseadBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class QuestionDemoController extends Controller
+{
+    public function questionDemo1Action()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
+       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+
+        return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
+            'q1' => $q1,
+           // 'choice' => $choice
+        ));
+    }
+
+    public function questionDemo2Action()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
+       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+
+        return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
+            'q1' => $q1,
+           // 'choice' => $choice
+        ));
+    }
+
+    public function questionDemo3Action()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
+       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+
+        return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
+            'q1' => $q1,
+           // 'choice' => $choice
+        ));
+    }
+
+    public function questionDemo4Action()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
+       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+
+        return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
+            'q1' => $q1,
+           // 'choice' => $choice
+        ));
+    }
+
+    public function questionDemo5Action()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
+       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+
+        return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
+            'q1' => $q1,
+           // 'choice' => $choice
+        ));
+    }
+
+
+}
