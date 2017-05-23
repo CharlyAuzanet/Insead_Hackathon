40d0b9d6733bf2e5b5444f1194004bebc4750b65
CharlyAuzanet <charly.auzanet@gmail.com>
2017-05-24 00:49:39 +0200

    question demo OK, manque la session...... et les form

--- src/InseadBundle/Controller/QuestionDemoController.php
+++ src/InseadBundle/Controller/QuestionDemoController.php
@@ -2,69 +2,105 @@
 
 namespace InseadBundle\Controller;
 
+use InseadBundle\Entity\Answer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class QuestionDemoController extends Controller
 {
-    public function questionDemo1Action()
+
+
+    public function questionDemo1Action(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('answer1', 'test');
+
         $em = $this->getDoctrine()->getManager();
-        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
-       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+        $question = $em->getRepository('InseadBundle:Questions')->getChoicesById(1);
 
         return $this->render('InseadBundle:user:questionDemo1.html.twig', array(
-            'q1' => $q1,
-           // 'choice' => $choice
+            'question' => $question
         ));
     }
 
-    public function questionDemo2Action()
+    public function questionDemo2Action(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('answer2', 'test');
+        
         $em = $this->getDoctrine()->getManager();
-        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
-       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+        $question = $em->getRepository('InseadBundle:Questions')->getChoicesById(2);
 
         return $this->render('InseadBundle:user:questionDemo2.html.twig', array(
-            'q1' => $q1,
-           // 'choice' => $choice
+            'question' => $question
         ));
     }
 
-    public function questionDemo3Action()
+    public function questionDemo3Action(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('answer3', 'test');
+        
         $em = $this->getDoctrine()->getManager();
-        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
-       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+        $question = $em->getRepository('InseadBundle:Questions')->getChoicesById(3);
 
         return $this->render('InseadBundle:user:questionDemo3.html.twig', array(
-            'q1' => $q1,
-           // 'choice' => $choice
+            'question' => $question
         ));
     }
 
-    public function questionDemo4Action()
+    public function questionDemo4Action(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('answer4', 'test');
+        
         $em = $this->getDoctrine()->getManager();
-        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
-       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+        $question = $em->getRepository('InseadBundle:Questions')->getChoicesById(4);
 
         return $this->render('InseadBundle:user:questionDemo4.html.twig', array(
-            'q1' => $q1,
-           // 'choice' => $choice
+            'question' => $question
         ));
     }
 
-    public function questionDemo5Action()
+    public function questionDemo5Action(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('answer5', 'test');
+        
         $em = $this->getDoctrine()->getManager();
-        $q1 = $em->getRepository('InseadBundle:Questions')->findOneById(1);
-       // $choice = $em->getRepository('InseadBundle:Choices')->findBy(array('questions_id' => 1));
+        $question = $em->getRepository('InseadBundle:Questions')->getChoicesById(5);
 
         return $this->render('InseadBundle:user:questionDemo5.html.twig', array(
-            'q1' => $q1,
-           // 'choice' => $choice
+            'question' => $question
         ));
     }
+    public function validerAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $request->getSession();
 
+        $answer = new Answer();
+        $answer->set($session->get('answer1'));
+        $answer = new Answer();
+        $answer->set($session->get('answer2'));
+        $answer = new Answer();
+        $answer->set($session->get('answer3'));
+        $answer = new Answer();
+        $answer->set($session->get('answer4'));
+        $answer = new Answer();
+        $answer->set($session->get('answer5'));
+
+
+        $em->persist($answer);
+        $em->flush();
+
+        $session->remove('answer1');
+        $session->remove('answer2');
+        $session->remove('answer3');
+        $session->remove('answer4');
+        $session->remove('answer5');
+
+        return $this->redirectToRoute('fos_user_registration_register');
+    }
 
 }
