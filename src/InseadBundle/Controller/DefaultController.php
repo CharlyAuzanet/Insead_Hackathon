6a44999f3f70ad2ea8df54138602bbdc388e50f4
JIM2022 <jaymail04@gmail.com>
2017-05-24 05:38:31 +0200

    views questionsAd OK

--- src/InseadBundle/Controller/DefaultController.php
+++ src/InseadBundle/Controller/DefaultController.php
@@ -6,13 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('InseadBundle:user:index.html.twig');
-    }
+
     public function rulesAction()
     {
         return $this->render('InseadBundle:user:rules.html.twig');
     }
 
+    public function adVideoAction()
+    {
+        return $this->render('InseadBundle:user:adVideo.html.twig');
+    }
+
+    public function questionAd1Action()
+    {
+        return $this->render('@Insead/user/questionAd1.html.twig');
+    }
+
+    public function questionAd2Action()
+    {
+        return $this->render('@Insead/user/questionAd2.html.twig');
+    }
+
+    public function questionAd3Action()
+    {
+        return $this->render('@Insead/user/questionAd3.html.twig');
+    }
 }
